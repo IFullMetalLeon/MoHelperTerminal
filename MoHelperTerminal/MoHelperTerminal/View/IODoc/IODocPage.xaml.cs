08171812bb63f3aa3e4dc5e719b014e5c0d29b8d
John <leon26@yandex.ru>
2021-07-07 15:13:57 +0800

    start

---
+++ MoHelperTerminal/MoHelperTerminal/View/IODoc/IODocPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoHelperTerminal.View.IODoc
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IODocPage : ContentPage
+    {
+        public IODocPage()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
---
+++ MoHelperTerminal/MoHelperTerminal/View/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoHelperTerminal.View.IODoc;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoHelperTerminal.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage(new IODocPage());
+        }
+    }
+}
